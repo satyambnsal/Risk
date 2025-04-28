c97f59e0f9ab38a3369d6e4b37546796c107161d
Satyam Bansal <satyamsgsits1994@gmail.com>
2025-04-28 14:18:18 +0530

    feat: Colesys integration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,39 @@
 'use client'
 
-import { Suspense } from 'react'
+import { Suspense, useEffect } from 'react'
 import dynamic from 'next/dynamic'
 
 // Load the Game component dynamically with no SSR
 const Game = dynamic(() => import('@/components/game/Game'), { ssr: false })
+import { Client, getStateCallbacks } from 'colyseus.js'
+
+async function connect() {
+  const client = new Client('http://localhost:2567')
+  const room = await client.joinOrCreate('my_room', {
+    /* custom join options */
+  })
+  const $ = getStateCallbacks(room)
+
+  // Listen to 'player' instance additions
+  $(room.state).players.onAdd((player, sessionId) => {
+    console.log('Player joined:', player)
+  })
+
+  // Listen to 'player' instance removals
+  $(room.state).players.onRemove((player, sessionId) => {
+    console.log('Player left:', player)
+  })
+
+  return room
+}
 
 // Load GameControls dynamically as well
 // const GameControls = dynamic(() => import('@/components/ui/GameControls'), { ssr: false })
 
 export default function Home() {
+  useEffect(() => {
+    connect()
+  }, [])
   return (
     <>
       <Suspense
